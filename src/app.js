27e8676b732a4974b9cfccc670d1bae0f9102c18
Lada_Ilyina <lada_ilyina@epam.com>
2018-12-02 17:32:01 +0300

    HW-1: log app name

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const models = require('./models');
+import config from '../config/config.json';
 
 const user = new models.user();
 const producr = new models.product();
+
+console.log(config.name);
