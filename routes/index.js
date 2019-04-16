25a1423a90cc1e9743bb82b1a9c9f8e22072d2b5
Lada_Ilyina <lada_ilyina@epam.com>
2019-04-16 13:59:59 +0300

    remove extra files

--- routes/index.js
+++
@@ -1,4 +0,0 @@
-export { default as usersRoutes } from './users'
-export { default as productsRoutes } from './products'
-export { default as authRouts } from './auth'
-export { default as citiesRoutes } from './cities'
